3b50839fb2c25980cb004756adc96cb9bbc27135
CozyMuncher <116059598+CozyMuncher@users.noreply.github.com>
2023-10-11 10:12:09 +0000

    modified scissors_cipher.py

--- scissors_cipher.py
+++ scissors_cipher.py
@@ -1,28 +1,28 @@
-message = input()
-message_list = list(message)  # You can directly convert the input string to a list of characters
-even_letters = []
-encrypted = ''
+while True:
+    message = input("Decrypted / Encrypted message:")
+    message_list = list(message)  # You can directly convert the input string to a list of characters
+    even_letters = []
+    encrypted = ''
 
-if len(message) % 2 == 0:
-    print('I am even (real)')
-    for counter in range(0, len(message), 2):
-        even_letters.append(message_list[counter])
-    odd_letters = [message_list[i] for i in range(1, len(message), 2)]
+    if len(message) % 2 == 0:
+        for counter in range(0, len(message), 2):
+            even_letters.append(message_list[counter])
+        odd_letters = [message_list[i] for i in range(1, len(message), 2)]        
 
-    for i in range(len(even_letters)):
-        encrypted += odd_letters[i]
-        encrypted += even_letters[i]
+        for i in range(len(even_letters)):
+            encrypted += odd_letters[i]
+            encrypted += even_letters[i]
 
-else:
-    print('I am even (fake)')
-    for counter in range(0, len(message) - 1, 2):
-        even_letters.append(message_list[counter])
-    odd_letters = [message_list[i] for i in range(1, len(message) - 1, 2)]
+    else:
+        for counter in range(0, len(message) - 1, 2):
+            even_letters.append(message_list[counter])
+        odd_letters = [message_list[i] for i in range(1, len(message) - 1, 2)]
+        odd_letters.append(message[-1])
 
-    for i in range(len(even_letters)):
-        encrypted += odd_letters[i]
-        encrypted += even_letters[i]
+        for i in range(len(even_letters)):
+            encrypted += odd_letters[i]
+            encrypted += even_letters[i]
 
-    encrypted += odd_letters[-1]
+        encrypted += odd_letters[-1]
 
-print(encrypted)
+    print(encrypted)
