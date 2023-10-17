90ba1f64c9967b9a9d3d14fdafe107df98abeae1
CozyMuncher <116059598+CozyMuncher@users.noreply.github.com>
2023-10-17 08:36:55 +0000

    Commented the code

--- caesar_cipher_decrypter.py
+++ caesar_cipher_decrypter.py
@@ -6,15 +6,24 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 while True:
     encrypted_text = input("Encrypted Message:")
     key = int(input("Key:"))
+
     DECRYPTED_TEXT = ""
     for char in encrypted_text:
+
+        # Checks if the character is a special char
         if char not in alphabet:
             DECRYPTED_TEXT += char
+
         else:
             INDEX = 0
+
+            # Checks if the index is greater than 26
+            # or less than 0 and changes the index accordingly
             if (alphabet.index(char)-key) < 0:
                 INDEX = alphabet.index(char)-key + 26
             else:
                 INDEX = alphabet.index(char)-key
+
+            # Adds the new character
             DECRYPTED_TEXT += alphabet[INDEX]
     print(f"Decrypted Text: {DECRYPTED_TEXT}")
