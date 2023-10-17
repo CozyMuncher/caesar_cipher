4fe4cc593100ff6040b5768fad3156755873b72b
CozyMuncher <116059598+CozyMuncher@users.noreply.github.com>
2023-10-17 08:38:37 +0000

    Commented the code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,15 +6,24 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 while True:
     decrypted_text = input("Decrypted Message:")
     key = int(input("Key:"))
+
     ENCRYPTED_TEXT = ""
+
     for char in decrypted_text:
+
+        # Checks if character is a special character
         if char not in alphabet:
             ENCRYPTED_TEXT += char
         else:
+
+            # Checks if the index is greater than 26
+            # or less than 0 and changes the index accordingly
             INDEX = 0
             if (alphabet.index(char)+key) > 26:
                 INDEX = alphabet.index(char)+key - 26
             else:
                 INDEX = alphabet.index(char)+key
+
             ENCRYPTED_TEXT += alphabet[INDEX]
+
     print(ENCRYPTED_TEXT)
