8b076af7a3ab7d2ab731e9154850f814b66a3ce8
TheInfamousPro <96720512+TheInfamousPro@users.noreply.github.com>
2023-10-17 16:10:19 +0800

    Made code shorter

--- scissors_cipher.py
+++ scissors_cipher.py
@@ -1,30 +1,19 @@
 """A cipher wich encrypt words by switching adjacent letters"""
+text = input('Text to encrypt:')
+char = list(text)
+encrypted_char = []
 
-while True:
-    message = input("Decrypted / Encrypted message:")
-    message_list = list(message)  # convert the input string to a list of characters
-    even_letters = []
-    ENCRYPTED = ''
+last_letter = ''
+if int(len(text) % 2) != 0:
+    print('removing last digit')
+    last_letter = char[-1]
+    del char[-1]
+    print(text)
 
-    if len(message) % 2 == 0:
-        for counter in range(0, len(message), 2):
-            even_letters.append(message_list[counter])
-        odd_letters = [message_list[i] for i in range(1, len(message), 2)]
+for counter in range(0, int(len(char)/2)):
+    encrypted_char.append(char[1])
+    encrypted_char.append(char[0])
+    del char[1]
+    del char[0]
 
-        for i in enumerate(len(even_letters)):
-            ENCRYPTED += odd_letters[i]
-            ENCRYPTED += even_letters[i]
-
-    else:
-        for counter in range(0, len(message) - 1, 2):
-            even_letters.append(message_list[counter])
-        odd_letters = [message_list[i] for i in range(1, len(message) - 1, 2)]
-        odd_letters.append(message[-1])
-
-        for i in enumerate(len(even_letters)):
-            ENCRYPTED += odd_letters[i]
-            ENCRYPTED += even_letters[i]
-
-        ENCRYPTED += odd_letters[-1]
-
-    print(ENCRYPTED)
+print(''.join(encrypted_char)+last_letter)
